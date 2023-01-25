dd73574c3ab455fe8d31954f51835bfc6601b207
wurikiji <wurikiji@gmail.com>
2023-01-25 16:54:20 +0900

    feat(animation): fade in out

---
+++ animation/fade_in_out/android/app/src/main/kotlin/com/example/fade_in_out/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fade_in_out
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ animation/fade_in_out/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: MyWidget()));
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  var _visible = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _visible ? 1.0 : 0.0,
+          duration: const Duration(milliseconds: 500),
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.green,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _visible = !_visible;
+          });
+        },
+      ),
+    );
+  }
+}
