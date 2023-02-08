3f7c85f5d5a2132913e74c60080b33428275e113
wurikiji <wurikiji@gmail.com>
2023-02-08 16:24:30 +0900

    unit-test: mockito package

---
+++ testing/unit_widget/lib/album.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  const Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
+
+Future<Album> fetchAlbum(http.Client client) async {
+  final response = await client.get(
+    Uri.parse('https://jsonplaceholder.typicode.com/albums/1'),
+  );
+  if (response.statusCode == 200) {
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
---
+++ testing/unit_widget/test/fetch_album_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:unit_widget/album.dart';
+
+import 'fetch_album_test.mocks.dart';
+
+@GenerateMocks([http.Client])
+void main() {
+  group('fetchAlbum', () {
+    test('returns an Album if the http call completes successfully', () async {
+      final client = MockClient();
+      when(
+        client.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')),
+      ).thenAnswer(
+        (_) async => http.Response(
+          '{"userId": 1, "id": 2, "title": "mock"}',
+          200,
+        ),
+      );
+      expect(await fetchAlbum(client), isA<Album>());
+    });
+
+    test('throws an exception if the http call completes with an error',
+        () async {
+      final client = MockClient();
+
+      when(
+        client.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')),
+      ).thenAnswer((_) async => http.Response('Not Found', 404));
+
+      expect(fetchAlbum(client), throwsException);
+    });
+  });
+}
