78dfe54279284d66a06be8d545bbcf645a0aba7d
wurikiji <wurikiji@gmail.com>
2023-02-08 15:41:10 +0900

    unit-test: grouping tests

--- testing/unit_widget/test/counter_test.dart
+++ testing/unit_widget/test/counter_test.dart
@@ -2,9 +2,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:unit_widget/counter.dart';
 
 void main() {
-  test('Counter value should be incremenetd', () {
-    final counter = Counter();
-    counter.increment();
-    expect(counter.value, 1);
+  group('Counter', () {
+    test('value should start at 0', () {
+      expect(Counter().value, equals(0));
+    });
+    test('value should be incremenetd', () {
+      final counter = Counter();
+      counter.increment();
+      expect(counter.value, 1);
+    });
+
+    test('value should be decremented', () {
+      final counter = Counter();
+      counter.decrement();
+      expect(counter.value, -1);
+    });
   });
 }
